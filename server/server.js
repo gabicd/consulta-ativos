ff46be13d70f9f016790e18e36f4796301d96a4b
gabicd <gabicd3323@gmail.com>
2025-10-21 20:04:10 -0300

    catalogar as datas que vao ter que ter os dados puxados pela API

--- server/server.js
+++ server/server.js
@@ -4,25 +4,31 @@ import fs from 'fs/promises';
 import path from 'path'
 import YahooFinance from "yahoo-finance2";
 import 'dotenv/config'
+import { autoc } from 'yahoo-finance2/modules';
 
 const app = express();  
 app.use(express.json());
 app.use(cors());
+const dataFilePath = './persistentData.json';
 const yahooFinance = new YahooFinance(); //api externa de cotação
 
 const PORT = 3000
+
+const isWeekend = date => date.getUTCDay() % 6 === 0;
 
 // rota para receber os dados do frontend e buscar na API externa
 app.post('/submit', async (req, res) => {
     const { assetArray, startDate, endDate } = req.body;
     const ativos = assetArray.flat();
-
     const endDateObj = new Date(endDate)
     endDateObj.setUTCDate(endDateObj.getUTCDate() + 1) //period2 nao inclui a data final, entao transforma em objeto para adicionar um dia na busca
     
     try {   // otimização com Promise.all para múltiplas requisições
         const saveData = {}
-        const promises = ativos.map(async ativo => {    
+        const searchDates = getIntervalDates(startDate, endDate)
+        const promises = ativos.map(async ativo => {
+        const missingInfo =  await findMissingDates(searchDates, ativo)
+        console.log(missingInfo)
         const symbol = `${ativo}.SA`    //adicionar o .SA para especificar que a busca é de um ativo na bolsa brasileira
 
             const response = await yahooFinance.chart(symbol, {
@@ -47,7 +53,7 @@ app.post('/submit', async (req, res) => {
     }
 );
         const results = await Promise.all(promises);
-        await fs.writeFile('./persistentData.json', JSON.stringify(saveData, null, 2))
+        //await fs.writeFile('./persistentData.json', JSON.stringify(saveData, null, 2))
         
         res.json({ data: results });
         //console.log('Fetched data:', results);
@@ -56,6 +62,44 @@ app.post('/submit', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch data from external API' });
         }
     });  
+
+function getIntervalDates(startDate, endDate) {
+    const days = []
+    let currentDate = new Date(`${startDate}T00:00:00Z`);
+    const finalDate = new Date(`${endDate}T00:00:00Z`);
+
+    while (currentDate <= finalDate) {
+        if (!(isWeekend(currentDate))) {
+            days.push(currentDate.toISOString().split('T')[0]);
+        }
+        currentDate.setUTCDate(currentDate.getUTCDate() + 1);
+    }
+    return days;
+
+}
+
+async function findMissingDates(searchDates, ativo) {
+    try {
+        const fileContent = await fs.readFile(dataFilePath, 'utf8')
+        const jsonData = JSON.parse(fileContent)
+
+        const assetData = jsonData[ativo] || []
+
+        const existingDates = new Set(assetData.map(record => record.date))
+
+        const missingDates = []
+
+        for (const date of searchDates) {
+            if (!(existingDates.has(date))) {
+                missingDates.push(date)
+            }
+        }
+
+        return missingDates
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
